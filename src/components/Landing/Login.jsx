0bc592023b6a1ca1d5a24a3eb27f2adade17c1cc
Manish Yadav <itp.manis@gmail.com>
2023-10-02 01:23:42 +0530

    done

--- src/components/Landing/Login.jsx
+++ src/components/Landing/Login.jsx
@@ -12,18 +12,22 @@ const Login = () => {
     // locally stored data li retrive gareko
     const storedSignupData = JSON.parse(localStorage.getItem('login'));
 
+
     if (storedSignupData) {
-      const { email: storedEmail, password: storedPassword } = storedSignupData;
+      const { email: storedEmail, password: storedPassword } = storedSignupData; // destructuring gareko ho. 
 
       // Check if the entered email and password match the stored signup data
       if (email === storedEmail && password === storedPassword) {
-        // Redirect to home if credentials match
-        navigate('/');
+        
         // Store the logged-in email and password in local storage
       localStorage.setItem('loggedInUser', JSON.stringify({ email, password }));
 
+      // login data li store garara home page ma redirect gareko
+      navigate('/');
+
+
       } else {
-        // Credentials do not match, handle accordingly (e.g., show an error message)
+        
         alert('Invalid email or password.');
       }
     } else {
@@ -34,53 +38,46 @@ const Login = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-1/3 bg-white p-8 rounded-lg shadow-lg">
-        <h2 className="text-2xl font-bold mb-4">Login</h2>
-        <form className="space-y-4" onSubmit={handleLogin}>
-          <div>
-            <label htmlFor="email" className="block mb-1">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Email"
-            />
-          </div>
-          <div>
-            <label htmlFor="password" className="block mb-1">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Password"
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full mt-4 px-6 py-3 bg-blue-500 text-white rounded-full hover:bg-blue-400 focus:outline-none"
-          >
-            Log in
-          </button>
-        </form>
-        <div className="mt-4 flex justify-between">
-          <Link to="/signup" className="text-blue-500 hover:underline">
-            Sign up
-          </Link>
-          <Link to="/forgotpassword" className="text-blue-500 hover:underline">
-            Forgot password?
-          </Link>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center" style={{ backgroundImage: 'url("https://images.pexels.com/photos/1640773/pexels-photo-1640773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")' }}>
+    <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
+      <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
+      <form className="space-y-4">
+        <div>
+          <label htmlFor="email" className="block mb-1 text-gray-700">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Email"
+          />
         </div>
+        <div>
+          <label htmlFor="password" className="block mb-1 text-gray-700">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Password"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full mt-4 px-6 py-3 bg-blue-500 text-white rounded-full hover:bg-blue-400 focus:outline-none transform transition-transform duration-300 ease-in-out hover:scale-105"
+          onClick={handleLogin}
+        >
+          Log in
+        </button>
+      </form>
+      <div className="mt-4 flex justify-between">
+        <Link to="/signup" className="text-blue-500 hover:underline">Sign up</Link>
+        <Link to="/forgotpassword" className="text-blue-500 hover:underline">Forgot password?</Link>
       </div>
     </div>
+  </div>
   );
 };
 
--- src/components/Landing/Signup.jsx
+++ src/components/Landing/Signup.jsx
@@ -32,81 +32,81 @@ const Signup = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="md:w-1/2 p-10">
-        <div className="mb-4">
-          Already a user? <Link to="/login" className="text-blue-500 hover:underline"><b>SignIn</b></Link>
-        </div>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              onChange={handleChange}
-              value={formValues.email}
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Email"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              onChange={handleChange}
-              value={formValues.password}
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Password"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="gender">
-              Gender
-            </label>
-            <select
-              name="gender"
-              id="gender"
-              onChange={handleChange}
-              value={formValues.gender}
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
-              <option value="">Select Gender</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-            </select>
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="company">
-              Company
-            </label>
-            <input
-              type="text"
-              name="company"
-              id="company"
-              onChange={handleChange}
-              value={formValues.company}
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Company"
-            />
-          </div>
-          <div className="mb-4">
-            <button
-              type="submit"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-            >
-              Register
-            </button>
-          </div>
-        </form>
+    <div className="flex justify-center items-center h-screen bg-cover bg-center" style={{ backgroundImage: 'url("https://images.pexels.com/photos/260922/pexels-photo-260922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")' }}>
+    <div className="md:w-1/3 p-10 bg-white bg-opacity-80 rounded-lg shadow-lg">
+      <div className="mb-4">
+        Already a user? <Link to="/login" className="text-blue-500 hover:underline"><b>SignIn</b></Link>
       </div>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={handleChange}
+            value={formValues.email}
+            className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Email"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={handleChange}
+            value={formValues.password}
+            className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Password"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="gender">
+            Gender
+          </label>
+          <select
+            name="gender"
+            id="gender"
+            onChange={handleChange}
+            value={formValues.gender}
+            className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
+            <option value="">Select Gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="company">
+            Company
+          </label>
+          <input
+            type="text"
+            name="company"
+            id="company"
+            onChange={handleChange}
+            value={formValues.company}
+            className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Company"
+          />
+        </div>
+        <div className="mb-4">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 transform hover:scale-105"
+          >
+            Register
+          </button>
+        </div>
+      </form>
     </div>
+  </div>
   );
 };
 
